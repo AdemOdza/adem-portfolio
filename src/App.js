494a3a4e8e9c1c323c833a8828d9b67fdd31114a
Adem Odza <adem@pop-os.localdomain>
2023-02-05 15:25:43 -0600

    Initial commit of refactor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import MyNavbar from './Components/MyNavbar/MyNavbar';
-import HomePage from './Components/HomePage/HomePage';
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-
 
 function App() {
   return (
-    
     <div className="App">
-      <MyNavbar />
-      <Router>
-        <Switch>
-          <Route path="/" exact component={HomePage}/>
-        </Switch>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
