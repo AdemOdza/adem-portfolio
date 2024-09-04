3a23fc646bb8afa7acd3281a91f4ea1b027bf47d
AdemOdza <AdemOdza@gmail.com>
2024-09-04 10:37:14 -0500

    Fixing another build error because I forgot to add a dependency

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Switch from '@mui/material/Switch';
-//import UsFlag from 'https://en.wikipedia.org/wiki/United_States#/media/File:Flag_of_the_United_States_(DoS_ECA_Color_Standard).svg';
-import ReactSVG from 'react';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+// import UsFlag from 'https://en.wikipedia.org/wiki/United_States#/media/File:Flag_of_the_United_States_(DoS_ECA_Color_Standard).svg';
+// import ReactSVG from 'react';
+// import Typography from '@mui/material/Typography';
 
-const label = { inputProps: { 'aria-label': 'Size switch demo' } };
+// const label = { inputProps: { 'aria-label': 'Size switch demo' } };
 
 const LanguageToggle = ({isEnglish, onToggle}: {isEnglish: boolean, onToggle: () => void}) => {
 
