b68ca2239e2091de9b0fe7a61f33c762940919ae
AdemOdza <AdemOdza@gmail.com>
2024-09-10 20:03:44 -0500

    Added contact page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {
 import Header from './components/Header';
 import ErrorPage from './pages/Error';
 import Home from './pages/Home';
+import { ContactPage } from './pages/Contact';
+import { Box, Modal, Typography } from '@mui/material';
 
 //Component reference: https://react-bootstrap.netlify.app/docs/components
 
@@ -24,11 +26,16 @@ const router = createBrowserRouter([
   {
     path: "/experience",
     element: <body>
-      Experience page to go here
+      TODO: Finish
+      resume garbage: this is where i have worked
       <p id="repairify"> repairify </p>
       <p id="astech"> astech </p>
       </body>,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: '/contact',
+    element: <ContactPage />
   },
   {
     path: "*",
@@ -40,16 +47,49 @@ const router = createBrowserRouter([
 
 function App() {
   const [isEnglish, setEnglish] = useState(true);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const toggleLanguage = () => {
     setEnglish(!isEnglish)
+    handleOpen()
   }
 
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  
   //Header
   //Toolbar
   //Body 
   return (
     <div>
       <Header isEnglish={isEnglish} toggleLanguage={toggleLanguage} />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            This isn&apos;t finished yet
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            This switch should switch the website&apos;s language between English and Shqip. <br/> <br/>
+            Click outside the box to exit
+          </Typography>
+        </Box>
+      </Modal>
+
       <React.StrictMode>
         <RouterProvider router={router}/> 
       </React.StrictMode>
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const ContactPage = () => {
+    return (
+        <body>
+            <h1>Contact Me</h1>
+            <div>
+                <h2>Email</h2>
+                <p> Contact me at info (at) AdemOdza (dot) com</p>
+            </div>
+            <div>
+                <h2>  <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/adem-odza/'>LinkedIn</a> </h2>
+                <p>
+                    Connect with me on LinkedIn
+                </p>
+            </div>
+        </body>
+    )
+}
