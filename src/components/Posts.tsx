0f9af8f08cfaa74d24db18710ed0b38779015c49
AdemOdza <AdemOdza@gmail.com>
2024-12-17 13:57:15 -0600

    Init Next.js rewrite

--- src/components/Posts.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-
-const Posts = () => {
-    return (<></>);
-};
-
-export default Posts
